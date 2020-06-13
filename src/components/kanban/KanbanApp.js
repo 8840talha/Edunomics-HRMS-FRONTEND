d8dcadbda45d76cf1103c2f8d2b94a6d9c66b355
infinity-intellect <snavaneetharan@gmail.com>
2020-06-13 18:44:55 +0530

    Made some color changes on the kanban board

--- src/components/kanban/KanbanApp.js
+++ src/components/kanban/KanbanApp.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import MainPage from './components/MainPage';
-
 function KanbanApp(props) {
   document.title = 'Kanban | Edunomics'
   document.body.style.backgroundColor = "white"
