65dd11bcd9a971c3177d4037d3b05202dbeb6601
talha <talhaparvez285@gmail.com>
2020-06-24 19:42:49 +0530

    button added to employee project

--- src/components/EmployeeContainer/TrackUpdateMydetailsMyrecords/TrackPage/Projects/project.js
+++ src/components/EmployeeContainer/TrackUpdateMydetailsMyrecords/TrackPage/Projects/project.js
@@ -13,11 +13,11 @@ const User = (props) => {
   // getting a view of an specific employee by id
   const loadProject = async () => {
     var token = localStorage.getItem('token');
-    fetch(`https://hrms-project.herokuapp.com/api/project/all`, { method: 'get', headers: { "Content-Type": "application/json", "Authorization": `Bearer ${token}`} })
-    .then(res => {
+    fetch(`https://hrms-project.herokuapp.com/api/project/all`, { method: 'get', headers: { "Content-Type": "application/json", "Authorization": `Bearer ${token}` } })
+      .then(res => {
         if (res.status !== 200 && res.status !== 201) {
-            console.log('hellllo');
-            throw new Error(res.status);
+          console.log('hellllo');
+          throw new Error(res.status);
         }
         return res.json();
 
@@ -25,52 +25,56 @@ const User = (props) => {
       .then(response => {
         console.log(response.project);
         setProjects(response.project);
-        
-        
-    })
+
+
+      })
       .catch(err => {
         console.log(err.message);
-        if(err.message == 404) {
-            // console.log('No projects found')
-            alert('No projects Found')
+        if (err.message == 404) {
+          // console.log('No projects found')
+          alert('No projects Found')
         } else {
-            // console.log('Some error occurred');
-            alert('Some error occurred');
-            
+          // console.log('Some error occurred');
+          alert('Some error occurred');
+
         }
-        
+
       })
 
   };
   return (
     <div style={{ marginTop: '100px', backgroundColor: '#fff' }} className="container py-4">
+      <Link style={{ height: '50px' }} className="btn btn-dark" to="/track">
+        Back to Home
+      </Link>
       <table style={{ backgroundColor: '#fff', width: '80%', marginLeft: '10%' }} className="table table-bordered shadow table-striped ">
-                    <thead className="thead">
-                        <tr>
-                            <th scope="col">#</th>
-                            <th scope="col">Project Name</th>
-                            <th>Action</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {projects.map((user, index) => (
-                          
-                            <tr key={index}>
-                                <th scope="row">{index + 1}</th>
-                                <td>{user.name}</td>
-                                <td>
-                                <Link className="btn btn-primary btn-block mr-2" 
-                                to={{pathname: "/kanban",
-                                state: {project: user}
-                                }}>
-                                        View Tasks
+        <thead className="thead">
+          <tr>
+            <th scope="col">#</th>
+            <th scope="col">Project Name</th>
+            <th>Action</th>
+          </tr>
+        </thead>
+        <tbody>
+          {projects.map((user, index) => (
+
+            <tr key={index}>
+              <th scope="row">{index + 1}</th>
+              <td>{user.name}</td>
+              <td>
+                <Link className="btn btn-primary btn-block mr-2"
+                  to={{
+                    pathname: "/kanban",
+                    state: { project: user }
+                  }}>
+                  View Tasks
                 </Link>
-                                </td>
-                            </tr>
-                        ))
-                        }
-                    </tbody>
-                </table>
+              </td>
+            </tr>
+          ))
+          }
+        </tbody>
+      </table>
 
     </div>
   );
